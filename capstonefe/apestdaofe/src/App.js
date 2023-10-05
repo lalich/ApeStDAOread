8646e0463d9a1156d9babcc1e1f7076a3081f4eb
Mark D. Lalich <mdlalich@gmail.com>
2023-10-05 11:46:38 -0700

    omg its alive!

--- capstonefe/apestdaofe/src/App.js
+++ capstonefe/apestdaofe/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Status from './components/status';
+import Send from './components/send';
+import Transactions from './components/transactions';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const endpoint = '/mine_block'
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentWillMount() {
+    axios.get(endpoint)
+  }
+  render(){
+    return (
+      <div className='App'>
+        <Status/>
+        <Send/>
+        <Transactions/>
+      </div>
+    )
+  }
+  }
 
 export default App;
---
+++ capstonefe/apestdaofe/src/components/send.js
@@ -0,0 +1,77 @@
+import React, { Component} from 'react';
+import { Form, Container, Col, Row, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+const postEndpoint = '/add_transaction'
+const getEndpoint = '/get_chain'
+class Send extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            recipient: '',
+            amount: 0,
+            time: '',
+            sender: '',
+        }
+        this.handleRecipient = this.handleRecipient.bind(this);
+        this.handleAmount = this.handleAmount.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleRecipient(event){
+        this.setState({ recipient: event.target.value});
+    }
+    handleAmount(event){
+        this.setState({ amount: event.target.value});
+    }
+    componentDidMount() {
+        axios.get(getEndpoint)
+            .then(res => {
+                const sender = res.data.chain[1].transactions[0].receiver;
+                this.setState({ sender });
+            })
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+
+            axios.post(postEndpoint, { 
+                'sender': this.state.sender,
+                'receiver': this.state.recipient,
+                'amount': this.state.amount,
+                'time': this.state.time })
+                    .then(res => {
+                        console.log(res);
+                        console.log(res.data);
+                    })
+    }
+    render(){
+        return (
+            <Container>
+                <br/>
+                <h3><b>ApeSt</b></h3>
+                <h4><b style={{color: '#007bff'}}>Send some funny money to anyone!</b></h4>
+                    <Form onSubmit={this.handleSubmit}>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm='2'>
+                            Amount
+                        </Form.Label>
+                        <Col sm='2'>
+                            <Form.Control onChange={this.handleAmount} placeholder='Amount' value={this.state.amount} />
+                        </Col>
+                        <Col sm='0.5'><b>ApeSt</b></Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Col sm='5'>
+                            <Button variant='primary' type='submit'>
+                                Send It!
+                            </Button>
+                        </Col>
+                    </Form.Group>
+                    </Form>
+                    <br/><br/>
+            </Container>
+        )
+    }
+}
+
+export default Send
