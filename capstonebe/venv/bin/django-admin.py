7de9d3a9922763a53474f3ee5e06b60c8cfa445a
Mark D. Lalich <mdlalich@gmail.com>
2023-10-10 18:12:35 -0700

    that was a close one!

---
+++ capstonebe/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/marklalich/Desktop/Desktop - Mark’s MacBook Pro/kale/capstone/capstonebe/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
