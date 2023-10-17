82cfe5bf87cf1276473b88f3d9bed4db04613e34
Mark D. Lalich <mdlalich@gmail.com>
2023-10-16 22:13:37 -0700

    tomorrow we ride

--- capstonefe/apestdaofe/src/pages/Jungle.js
+++ capstonefe/apestdaofe/src/pages/Jungle.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { Form, Link } from 'react-router-dom'
-import Header from '../components/Header'
-import './pagestyle.css'
+import { Form, Link } from 'react-router-dom';
+import Header from '../components/Header';
+import './pagestyle.css';
+import { ethers } from 'ethers';
+import axios from 'axios';
 
 const Jungle = () => {
+    const ApeStFFcreate = async (data) => {
+            try {
+                const formData = {
+                    name: "Project Name",
+                    ein: "EIN Value",
+                    locationZip: "Zip Code",
+                    description: "Project Description",
+                    founder: "Founder Address",
+                    website: "Website URL",
+                    jungle: "",
+                    tree: "",
+                    node: "",
+
+                }
+                const response = await axios.post('create_apestff', data);
+                console.log(response.data.result);
+                
+            } catch (error) {
+                console.error('Error this time', error)
+            }
+
+    }
+
     return <>
         <div>
         <div className='header'>
@@ -11,7 +36,7 @@ const Jungle = () => {
         </div>
         
         <h1 style={{textAlign: "center", color: 'black', backgroundColor: '#378b20'}}>Welcome to the Jungle</h1>
-        <Form method='post' action='/create/apestff/'>
+        <Form method='post' action='/create/ApeStFF'>
             <div className='fform'>
         What is the Name of your project?
         <input type='text' name='name' placeholder='Project Name'/>
@@ -42,7 +67,7 @@ const Jungle = () => {
         or a Node
         <input type='checkbox' name='node' checkbox='empty'/>
         <br/>
-        <button className='projsub' type="submit">Upload Project to the Chain for Review</button>
+        <button onClick={ApeStFFcreate} className='projsub' type="submit">Upload Project to the Chain for Review</button>
         </div>
 
         </Form>
